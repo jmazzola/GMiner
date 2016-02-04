c0eb4e59b6a53afa6550133edfc999054018126c
Justin Mazzola <mazzolajustin@gmail.com>
2016-02-04 18:49:34 -0500

    ye

---
+++ GMiner/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace GMiner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.treeViewFile = new System.Windows.Forms.TreeView();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(742, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open .win";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 398);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(742, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(64, 17);
+            this.toolStripStatusLabel1.Text = "Status: Idle";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // ofd
+            // 
+            this.ofd.FileName = "openFileDialog1";
+            this.ofd.Filter = ".win Files | *.win";
+            this.ofd.Title = "Open your GameMaker .win file";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.progressBar1);
+            this.panel1.Location = new System.Drawing.Point(2, 369);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(739, 29);
+            this.panel1.TabIndex = 2;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(3, 3);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(733, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(279, 32);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(451, 327);
+            this.panel3.TabIndex = 4;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(10, 32);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(263, 331);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.treeViewFile);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(255, 305);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "File Hierarchy";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // treeViewFile
+            // 
+            this.treeViewFile.Location = new System.Drawing.Point(6, 6);
+            this.treeViewFile.Name = "treeViewFile";
+            this.treeViewFile.Size = new System.Drawing.Size(243, 293);
+            this.treeViewFile.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 420);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "GMiner - Open Source GameMaker Decompiler/Dumper";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TreeView treeViewFile;
+    }
+}
+
---
+++ GMiner/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace GMiner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // The file currently opened
+        private string filePath = "";
+
+        public enum SectionHeader : uint
+        {
+            Form        = 0x4D524F46, // FORM
+            General     = 0x384E4547, // GEN8
+            Options     = 0x4E54504F, // OPTN
+            Extensions  = 0x4E545845, // EXTN
+            Sounds      = 0x444E4F53, // SOND
+            AudioGroup  = 0x50524741, // AGRP
+            Sprites     = 0x54525053, // SPRT
+            Backgrounds = 0x444E4742, // BGND
+            Paths       = 0x48544150, // PATH
+            Scripts     = 0x54504353, // SCPT
+            Shaders     = 0x52444853, // SHDR
+            Fonts       = 0x544E4F46, // FONT
+            Timelines   = 0x4E4C4D54, // TMLN
+            Objects     = 0x544A424F, // OBJT
+            Rooms       = 0x4D4F4F52, // ROOM
+            DataFiles   = 0x4C464144, // DAFL
+            TexturePage = 0x47415054, // TPAG
+            Code        = 0x45444F43, // CODE
+            Variables   = 0x49524156, // VARI
+            Functions   = 0x434E5546, // FUNC
+            Strings     = 0x47525453, // STRG
+            Textures    = 0x52545854, // TXTR
+            Audio       = 0x4F445541, // AUDO
+
+            Count = 23,
+         }
+
+        public enum GeneralOptionInfoFlags : uint
+        {
+            Fullscreen = 0x1,
+            SyncVertex_And1 = 0x2,              // (class5_0.Options.Sync_Vertex & 1) != 0
+            SyncVertex_AndNegMaxNum = 0x4,      // ((long)class5_0.Options.Sync_Vertex & (ulong)-2147483648) != (long)0
+            InterpolatePixels = 0x8,
+            NegativeScale = 0x10,
+            ShowCursor = 0x20,
+            Sizeable = 0x40,
+            ScreenKey = 0x80,
+            SyncVertex_And4Million = 0x100,     // (class5_0.Options.Sync_Vertex & 0x40000000) != 0
+            StudioEdition_Equals1 = 0x200,
+            StudioEdition_Equals2 = 0x400,
+            StudioEdition_Equals3 = 0x800,
+            StudioEdition_Equals4 = 0x600,
+            SteamProject_OrYoyoPlayer = 0x1000, // if (Class168.SteamProject) || if (Class168.MachineType.Name == "YoYoPlayer")
+            SaveLocation = 0x2000,
+            Borderless = 0x4000,
+        }
+
+        public struct Chunk
+        {
+            SectionHeader header;
+            uint sizeOfChunk;
+        }
+
+        public unsafe struct GeneralInfo
+        {
+            public Chunk chunk;
+            public int debug;                                   // (class5_0.Debug ? 0 : 1) | 0xe00
+            public int fileNameWithoutExtensionOffset;     
+            public int optionsConfigNameOffset;                 // class5_0.Options.Config
+            public int roomMaxID;
+            public int roomMaxTileID;
+            public int gameID;
+            public fixed uint padding0[4];
+            public int gameNameOffset;
+            public int majorVersion;
+            public int minorVersion;
+            public int releaseVersion;
+            public int buildVersion;
+            public Point windowSize;
+            public GeneralOptionInfoFlags flags;
+            public int CRC;
+            public fixed byte CRC32Checksum[16];
+            public double totalSecondsTimestamp;
+            public int displayNameOffset;
+            public int activeTargets_AndNeg1;                   // class5_0.Options.ActiveTargets & (ulong)-1
+            public int activeTargets_SHR32AndNeg1;              // class5_0.Options.ActiveTargets >> 32 & (ulong)-1
+            public int functionClassifications_AndNeg1;         // class5_0.FunctionClassifications & (ulong)-1
+            public int functionClassifications_SHR32AndNeg1;    // class5_0.FunctionClassifications & (ulong)-1
+            public int steamAppID;
+            public int debuggerPort;
+            public int roomOrderCount;
+            public int roomNumbers;                             // It literally writes 0-roomCount for some reason.
+        }
+
+        private void UpdateStatus(string status)
+        {
+            toolStripStatusLabel1.Text = status;
+            statusStrip1.Update();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ofd.ShowDialog() != DialogResult.OK)
+                return;
+
+            filePath = ofd.FileName;
+
+            if (filePath != "")
+                ParseFile();
+        }
+
+        private void ParseFile()
+        {
+            using (BinaryReader br = new BinaryReader(File.OpenRead(filePath)))
+            {
+                if (br.ReadInt32() != (int)SectionHeader.Form)
+                {
+                    UpdateStatus("Not a compatible .win file.");
+                }
+
+            }
+        }
+    }
+}
